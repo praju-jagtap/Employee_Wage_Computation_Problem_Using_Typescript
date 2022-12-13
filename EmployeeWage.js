46bf58da91a7b10245f6766169139879662ceedd
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-12-13 12:57:24 +0530

    [Prajakta]Added: Calculate Daily Employee Wages

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,14 @@
-//UC1-Emplooyee is Present or Absent
-var IS_ABSENT = 0;
-var empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent");
-}
-else {
-    console.log("Employee is Present");
-}
+//UC2-Calculate Employee Daily Wage
+var EmpTimeDetail;
+(function (EmpTimeDetail) {
+    EmpTimeDetail[EmpTimeDetail["IS_FULL_TIME"] = 1] = "IS_FULL_TIME";
+    EmpTimeDetail[EmpTimeDetail["WAGE_PER_HOUR"] = 20] = "WAGE_PER_HOUR";
+})(EmpTimeDetail || (EmpTimeDetail = {}));
+var empHrs = 0;
+var empCheck = Math.floor(Math.random() * 10) % 3;
+if (empCheck == EmpTimeDetail.IS_FULL_TIME)
+    empHrs = 8;
+else
+    empCheck = 0;
+var empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+console.log("Employee Daily Wage is : " + empWage);
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -1,10 +1,17 @@
-//UC1-Emplooyee is Present or Absent
+//UC2-Calculate Employee Daily Wage
 
-const IS_ABSENT:number = 0 
-
-let empCheck:number = Math.floor(Math.random() * 10) % 2;
-if(empCheck == IS_ABSENT) {
-    console.log("Employee is Absent");
-} else {
-    console.log("Employee is Present");
+enum EmpTimeDetail{
+    IS_FULL_TIME = 1,
+    WAGE_PER_HOUR = 20
 }
+
+let empHrs:number = 0;
+let empCheck:number = Math.floor(Math.random() * 10) % 3;
+
+if(empCheck == EmpTimeDetail.IS_FULL_TIME)
+   empHrs = 8;
+else
+   empCheck = 0;
+   
+let empWage:number = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+console.log("Employee Daily Wage is : " +empWage);

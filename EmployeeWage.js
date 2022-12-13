d290991a4d686a65b3e811bae9370a4a456ed334
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-12-13 13:32:38 +0530

    [Prajakta]Added: Create Method For Calculate Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-//UC6-Calculate Wage till No of working days is reached
+//UC7-Employee Wages For month using Function
 var EmpTimeDetail;
 (function (EmpTimeDetail) {
     EmpTimeDetail[EmpTimeDetail["IS_FULL_TIME"] = 1] = "IS_FULL_TIME";
@@ -7,27 +7,32 @@ var EmpTimeDetail;
     EmpTimeDetail[EmpTimeDetail["FULL_TIME_HOUR"] = 8] = "FULL_TIME_HOUR";
     EmpTimeDetail[EmpTimeDetail["WAGE_PER_HOUR"] = 20] = "WAGE_PER_HOUR";
     EmpTimeDetail[EmpTimeDetail["NUM_OF_WORKING_DAYS"] = 20] = "NUM_OF_WORKING_DAYS";
-    EmpTimeDetail[EmpTimeDetail["MAX_HRS_IN_MONTH"] = 100] = "MAX_HRS_IN_MONTH";
+    EmpTimeDetail[EmpTimeDetail["MAX_HRS_IN_MONTH"] = 160] = "MAX_HRS_IN_MONTH";
 })(EmpTimeDetail || (EmpTimeDetail = {}));
 var empHrs = 0;
 var totalEmployeeWage = 0;
 var empWage = 0;
 var totalEmpHour = 0;
 var totalWorkingDays = 0;
-while (totalEmpHour <= EmpTimeDetail.MAX_HRS_IN_MONTH && totalWorkingDays < EmpTimeDetail.NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    var empCheck = Math.floor(Math.random() * 10) % 3;
-    switch (empCheck) {
-        case EmpTimeDetail.IS_PART_TIME:
-            empHrs = EmpTimeDetail.PART_TIME_HOURS;
-            break;
-        case EmpTimeDetail.IS_FULL_TIME:
-            empHrs = EmpTimeDetail.FULL_TIME_HOUR;
-            break;
-        default:
-            empHrs = 0;
+function getworkingHours() {
+    while (totalEmpHour <= EmpTimeDetail.MAX_HRS_IN_MONTH &&
+        totalWorkingDays < EmpTimeDetail.NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        var empCheck = Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case EmpTimeDetail.IS_PART_TIME:
+                empHrs = EmpTimeDetail.PART_TIME_HOURS;
+                break;
+            case EmpTimeDetail.IS_FULL_TIME:
+                empHrs = EmpTimeDetail.FULL_TIME_HOUR;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+        totalEmployeeWage += empWage;
     }
-    empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
-    totalEmployeeWage += empWage;
+    return totalEmployeeWage;
 }
-console.log("Total Days : " + totalWorkingDays + "\t\tTotal Employee Wage: " + empWage);
+var result = getworkingHours();
+console.log("Total Employee Wages For Month: ", result);
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -1,4 +1,4 @@
-//UC6-Calculate Wage till No of working days is reached
+//UC7-Employee Wages For month using Function
 
 enum EmpTimeDetail {
   IS_FULL_TIME = 1,
@@ -7,7 +7,7 @@ enum EmpTimeDetail {
   FULL_TIME_HOUR = 8,
   WAGE_PER_HOUR = 20,
   NUM_OF_WORKING_DAYS = 20,
-  MAX_HRS_IN_MONTH = 100
+  MAX_HRS_IN_MONTH = 160
 }
 
 let empHrs: number = 0;
@@ -16,20 +16,25 @@ let empWage: number = 0;
 let totalEmpHour: number = 0;
 let totalWorkingDays: number = 0;
 
-while (totalEmpHour <= EmpTimeDetail.MAX_HRS_IN_MONTH && totalWorkingDays < EmpTimeDetail.NUM_OF_WORKING_DAYS) {
-  totalWorkingDays++;
-  let empCheck: number = Math.floor(Math.random() * 10) % 3;
-  switch (empCheck) {
-    case EmpTimeDetail.IS_PART_TIME:
-      empHrs = EmpTimeDetail.PART_TIME_HOURS;
-      break;
-    case EmpTimeDetail.IS_FULL_TIME:
-      empHrs = EmpTimeDetail.FULL_TIME_HOUR;
-      break;
-    default:
-      empHrs = 0;
+function getworkingHours(): number {
+  while (totalEmpHour <= EmpTimeDetail.MAX_HRS_IN_MONTH &&
+    totalWorkingDays < EmpTimeDetail.NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck: number = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+      case EmpTimeDetail.IS_PART_TIME:
+        empHrs = EmpTimeDetail.PART_TIME_HOURS;
+        break;
+      case EmpTimeDetail.IS_FULL_TIME:
+        empHrs = EmpTimeDetail.FULL_TIME_HOUR;
+        break;
+      default:
+        empHrs = 0;
+    }
+    empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+    totalEmployeeWage += empWage;
   }
-  empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
-  totalEmployeeWage += empWage;
+  return totalEmployeeWage;
 }
-console.log("Total Days : " + totalWorkingDays + "\t\tTotal Employee Wage: " + empWage);
+let result: number = getworkingHours();
+console.log("Total Employee Wages For Month: ", result);

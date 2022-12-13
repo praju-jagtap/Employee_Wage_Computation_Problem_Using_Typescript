61bc8b111202b4787d7aaebe6ed4a4075b11e4b7
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-12-13 13:19:21 +0530

    [Prajakta]Added: Calculate Employee Wages For Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-//UC4-Calculate Wages using switch case
+//UC5-Calculate Employee Wage For Month
 var EmpTimeDetail;
 (function (EmpTimeDetail) {
     EmpTimeDetail[EmpTimeDetail["IS_FULL_TIME"] = 1] = "IS_FULL_TIME";
@@ -6,18 +6,25 @@ var EmpTimeDetail;
     EmpTimeDetail[EmpTimeDetail["PART_TIME_HOURS"] = 4] = "PART_TIME_HOURS";
     EmpTimeDetail[EmpTimeDetail["FULL_TIME_HOUR"] = 8] = "FULL_TIME_HOUR";
     EmpTimeDetail[EmpTimeDetail["WAGE_PER_HOUR"] = 20] = "WAGE_PER_HOUR";
+    EmpTimeDetail[EmpTimeDetail["NUM_OF_WORKING_DAYS"] = 20] = "NUM_OF_WORKING_DAYS";
 })(EmpTimeDetail || (EmpTimeDetail = {}));
 var empHrs = 0;
-var empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case EmpTimeDetail.IS_PART_TIME:
-        empHrs = EmpTimeDetail.PART_TIME_HOURS;
-        break;
-    case EmpTimeDetail.IS_FULL_TIME:
-        empHrs = EmpTimeDetail.FULL_TIME_HOUR;
-        break;
-    default:
-        empHrs = 0;
+var totalEmployeeWage = 0;
+var empWage = 0;
+for (var day = 0; day < EmpTimeDetail.NUM_OF_WORKING_DAYS; day++) {
+    var empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+        case EmpTimeDetail.IS_PART_TIME:
+            empHrs = EmpTimeDetail.PART_TIME_HOURS;
+            break;
+        case EmpTimeDetail.IS_FULL_TIME:
+            empHrs = EmpTimeDetail.FULL_TIME_HOUR;
+            break;
+        default:
+            empHrs = 0;
+    }
+    empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+    totalEmployeeWage += empWage;
+    console.log("Employee Wage is: " + empWage);
 }
-var empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
-console.log("Employee Wage is : " + empWage);
+console.log("Total Employee Wage of Month: " + totalEmployeeWage);
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -1,26 +1,32 @@
-//UC4-Calculate Wages using switch case
+//UC5-Calculate Employee Wage For Month
 
 enum EmpTimeDetail {
   IS_FULL_TIME = 1,
   IS_PART_TIME = 2,
   PART_TIME_HOURS = 4,
   FULL_TIME_HOUR = 8,
-  WAGE_PER_HOUR = 20
+  WAGE_PER_HOUR = 20,
+  NUM_OF_WORKING_DAYS = 20
 }
 
 let empHrs: number = 0;
-let empCheck: number = Math.floor(Math.random() * 10) % 3;
+let totalEmployeeWage: number = 0;
+let empWage: number = 0;
 
-switch (empCheck) {
-  case EmpTimeDetail.IS_PART_TIME:
-    empHrs = EmpTimeDetail.PART_TIME_HOURS;
-    break;
-  case EmpTimeDetail.IS_FULL_TIME:
-    empHrs = EmpTimeDetail.FULL_TIME_HOUR;
-    break;
-  default:
-    empHrs = 0;
+for (let day: number = 0; day < EmpTimeDetail.NUM_OF_WORKING_DAYS; day++) {
+  let empCheck: number = Math.floor(Math.random() * 10) % 3;
+  switch (empCheck) {
+    case EmpTimeDetail.IS_PART_TIME:
+      empHrs = EmpTimeDetail.PART_TIME_HOURS;
+      break;
+    case EmpTimeDetail.IS_FULL_TIME:
+      empHrs = EmpTimeDetail.FULL_TIME_HOUR;
+      break;
+    default:
+      empHrs = 0;
+  }
+  empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+  totalEmployeeWage += empWage;
+  console.log("Employee Wage is: " + empWage);
 }
-
-let empWage: number = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
-console.log("Employee Wage is : " + empWage);
+console.log("Total Employee Wage of Month: " + totalEmployeeWage);

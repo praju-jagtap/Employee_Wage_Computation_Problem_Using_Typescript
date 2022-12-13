23518f2dc43c36b912e524b6e893b82f92da390d
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-12-13 13:12:51 +0530

    [Prajakta]Added: Calculate Wages Using Switch-Case

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,17 +1,23 @@
-//UC3-Added Part Time Wages
+//UC4-Calculate Wages using switch case
 var EmpTimeDetail;
 (function (EmpTimeDetail) {
     EmpTimeDetail[EmpTimeDetail["IS_FULL_TIME"] = 1] = "IS_FULL_TIME";
     EmpTimeDetail[EmpTimeDetail["IS_PART_TIME"] = 2] = "IS_PART_TIME";
+    EmpTimeDetail[EmpTimeDetail["PART_TIME_HOURS"] = 4] = "PART_TIME_HOURS";
+    EmpTimeDetail[EmpTimeDetail["FULL_TIME_HOUR"] = 8] = "FULL_TIME_HOUR";
     EmpTimeDetail[EmpTimeDetail["WAGE_PER_HOUR"] = 20] = "WAGE_PER_HOUR";
 })(EmpTimeDetail || (EmpTimeDetail = {}));
 var empHrs = 0;
 var empCheck = Math.floor(Math.random() * 10) % 3;
-if (empCheck === EmpTimeDetail.IS_PART_TIME)
-    empHrs = 4;
-else if (empCheck === EmpTimeDetail.IS_FULL_TIME)
-    empCheck = 8;
-else
-    empCheck = 0;
+switch (empCheck) {
+    case EmpTimeDetail.IS_PART_TIME:
+        empHrs = EmpTimeDetail.PART_TIME_HOURS;
+        break;
+    case EmpTimeDetail.IS_FULL_TIME:
+        empHrs = EmpTimeDetail.FULL_TIME_HOUR;
+        break;
+    default:
+        empHrs = 0;
+}
 var empWage = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
 console.log("Employee Wage is : " + empWage);
--- EmployeeWage.ts
+++ EmployeeWage.ts
@@ -1,20 +1,26 @@
-//UC3-Added Part Time Wages
+//UC4-Calculate Wages using switch case
 
-enum EmpTimeDetail{
-   IS_FULL_TIME = 1,
-   IS_PART_TIME = 2,
-   WAGE_PER_HOUR = 20
+enum EmpTimeDetail {
+  IS_FULL_TIME = 1,
+  IS_PART_TIME = 2,
+  PART_TIME_HOURS = 4,
+  FULL_TIME_HOUR = 8,
+  WAGE_PER_HOUR = 20
 }
 
-let empHrs:number = 0;
-let empCheck:number = Math.floor(Math.random() * 10) % 3;
+let empHrs: number = 0;
+let empCheck: number = Math.floor(Math.random() * 10) % 3;
 
-if(empCheck === EmpTimeDetail.IS_PART_TIME)
-  empHrs = 4;
-else if(empCheck === EmpTimeDetail.IS_FULL_TIME)
-  empCheck = 8;
-else
-   empCheck = 0;
-   
-let empWage:number = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
-console.log("Employee Wage is : " +empWage);
+switch (empCheck) {
+  case EmpTimeDetail.IS_PART_TIME:
+    empHrs = EmpTimeDetail.PART_TIME_HOURS;
+    break;
+  case EmpTimeDetail.IS_FULL_TIME:
+    empHrs = EmpTimeDetail.FULL_TIME_HOUR;
+    break;
+  default:
+    empHrs = 0;
+}
+
+let empWage: number = empHrs * EmpTimeDetail.WAGE_PER_HOUR;
+console.log("Employee Wage is : " + empWage);
